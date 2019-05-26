cfd856ebcf285e75ba062a5fe6c812a077f96c4b
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-26 09:49:32 +0300

    Add files via upload

---
+++ Lesson 1/Messages.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _5._Messages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int letters = int.Parse(Console.ReadLine());
+            int numberDigits = 0;
+            int mainDigit = 0;
+            int offset = 0;
+            int letterIndex = 0;
+            char letter;
+            string sms = string.Empty;
+
+            for (int i = 0; i < letters; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number / 1000 != 0)
+                {
+                    numberDigits = 4;
+                }
+                else if (number / 100 != 0)
+                {
+                    numberDigits = 3;
+                }
+                else if (number / 10 != 0)
+                {
+                    numberDigits = 2;
+                }
+                else
+                {
+                    numberDigits = 1;
+                }
+                mainDigit = number % 10;
+                offset = (mainDigit - 2) * 3;
+                if (mainDigit == 8 || mainDigit == 9)
+                {
+                    offset++;
+                }
+                letterIndex = (offset + numberDigits - 1);
+                if (mainDigit!=0)
+                {
+                    letter = (char)(97 + letterIndex);
+                }
+                else
+                {
+                    letter = (char)32;
+                }
+                sms += letter;
+            }
+
+            Console.WriteLine(sms);
+        }
+    }
+}
