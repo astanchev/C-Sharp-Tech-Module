2b32c5d42fa64b3a8a436d007b0adad0d77fc75b
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-26 09:48:15 +0300

    Add files via upload

---
+++ Lesson 1/Sort_Numbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1._Sort_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double first = double.Parse(Console.ReadLine());
+            double second = double.Parse(Console.ReadLine());
+            double third = double.Parse(Console.ReadLine());
+            double max = Math.Max(Math.Max(first, second), third);
+            double min = Math.Min(Math.Min(first, second), third);
+            double middle = (first + second + third) - (max + min); ;
+
+            Console.WriteLine(max);
+            Console.WriteLine(middle);
+            Console.WriteLine(min);
+            
+        }
+    }
+}
