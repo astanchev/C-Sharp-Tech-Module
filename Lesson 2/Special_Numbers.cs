85ad1e3800fd56c314489be41db842d66cfbdf4b
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-27 08:48:29 +0300

    Add files via upload

---
+++ Lesson 2/Special_Numbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int num = 1; num <= n; num++)
+            {
+                int sumOfDigits = 0;
+                int digits = num;
+                while (digits > 0)
+                {
+                    sumOfDigits += digits % 10;
+                    digits = digits / 10;
+                }
+                if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+                {
+                    Console.WriteLine($"{num} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{num} -> False");
+                }
+            }
+
+        }
+    }
+}
