be8dba14929e7228d7cbd7aec317921f30d5db8d
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-27 08:46:17 +0300

    Add files via upload

---
+++ Lesson 2/Convert_Meters_to_Kilometers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01._Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{meters / 1000d:f2}");
+        }
+    }
+}
