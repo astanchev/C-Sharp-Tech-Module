69cb748c129a83331e16415c8ef479e55c577d2f
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-27 08:53:09 +0300

    Add files via upload

---
+++ Lesson 2/Refactor_Special_Numbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());    
+            for (int currentNum = 1; currentNum <= numbers; currentNum++)
+            {
+                int sumOfDigits = 0;
+                int digits = currentNum;
+                while (digits > 0)
+                {
+                    sumOfDigits += digits % 10;
+                    digits = digits / 10;
+                }
+                bool isSpecialNum = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11);
+                Console.WriteLine($"{currentNum} -> {isSpecialNum}");
+                }
+        }
+    }
+}
