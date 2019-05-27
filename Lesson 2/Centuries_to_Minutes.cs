fdd271b4eaeecb44183836ba4f239d1973a2a245
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-27 08:47:57 +0300

    Add files via upload

---
+++ Lesson 2/Centuries_to_Minutes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04._Centuries_to_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * 100;
+            int days = (int)(years * 365.2422);
+            int hours = 24 * days;
+            int minutes = 60 * hours;
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+
+        }
+    }
+}
