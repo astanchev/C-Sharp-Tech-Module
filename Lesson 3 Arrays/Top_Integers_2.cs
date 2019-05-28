932ff0c99df66a7dbf0fb04df066eec4b071fa70
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-29 00:14:47 +0300

    Add files via upload

---
+++ Lesson 3 Arrays/Top_Integers_2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._2_Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool isTopInteger = true;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] <= array[j])
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+                if (isTopInteger == true)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+            }
+        }
+    }
+}
