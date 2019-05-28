5e15a9e7f455347396be24d81a6f837cfe608144
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-28 07:15:37 +0300

    Add files via upload

---
+++ Lesson 3 Arrays/Common_Elements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = Console.ReadLine().Split(' ').ToArray();
+            string[] array2 = Console.ReadLine().Split(' ').ToArray();
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                for (int j = 0; j < array1.Length; j++)
+                {
+                    if (array2[i]==array1[j])
+                    {
+                        Console.Write($"{array2[i]} ");
+                    }
+                }
+            }
+
+
+        }
+    }
+}
