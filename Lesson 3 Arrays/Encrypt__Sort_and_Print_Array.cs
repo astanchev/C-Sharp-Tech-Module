d0426558ff7f9583974c4903fa76a87f918370a4
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-28 07:13:51 +0300

    Add files via upload

---
+++ Lesson 3 Arrays/Encrypt__Sort_and_Print_Array.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Encrypt__Sort_and_Print_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStrings = int.Parse(Console.ReadLine());
+            int[] numberSequences = new int[numberOfStrings];
+            
+
+            char[] vowels = { 'a', 'A', 'o', 'O', 'e', 'E',
+                              'i', 'I', 'u', 'U'/*, 'y', 'Y'*/};
+
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                string input = Console.ReadLine();
+                char[] inputChars = input.ToCharArray();
+                int sumSequence = 0;
+                for (int j = 0; j < inputChars.Length; j++)
+                {
+                    if (vowels.Contains(inputChars[j]))
+                    {
+                        sumSequence += inputChars[j] * input.Length;
+                    }
+                    else
+                    {
+                        sumSequence += inputChars[j] / input.Length;
+                    }
+                }
+                numberSequences[i] = sumSequence;
+            }
+            Array.Sort(numberSequences);
+            for (int i = 0; i < numberSequences.Length; i++)
+            {
+                Console.WriteLine(numberSequences[i]);
+            }
+        }
+    }
+}
