531f32d03359cc616cddb1d6ba6f971d1b5c4ca1
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-29 00:15:27 +0300

    Add files via upload

---
+++ Lesson 3 Arrays/Equal_Sum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Equal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            
+                for (int i = 0; i < array.Length; i++)
+                {
+                    int leftSum = 0;
+                    int rightSum = 0;
+                    for (int leftIndex = 0; leftIndex < i; leftIndex++)
+                    {
+                        leftSum += array[leftIndex];
+                    }
+                    for (int rightIndex = i + 1; rightIndex < array.Length; rightIndex++)
+                    {
+                        rightSum += array[rightIndex];
+                    }
+                    if (leftSum == rightSum)
+                    {
+                        Console.WriteLine(i);
+                        return;                        
+                    }
+                }
+                Console.WriteLine("no");
+            
+
+        }
+    }
+}
