4ce6c7a6fa75c923724a3d1e7d1cec34ef6ce03c
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-28 07:14:31 +0300

    Add files via upload

---
+++ Lesson 3 Arrays/Train.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+
+            int[] passangersInWagon = new int[wagons];
+            int allPassangers = 0;
+
+            for (int i = 0; i < passangersInWagon.Length; i++)
+            {
+                passangersInWagon[i] = int.Parse(Console.ReadLine());
+                allPassangers += passangersInWagon[i];
+            }
+
+            Console.WriteLine(string.Join(" ",passangersInWagon));
+            Console.WriteLine(allPassangers);
+        }
+    }
+}
