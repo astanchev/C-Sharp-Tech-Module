cca3ab74b946d45185f755ff59ae2797aae7231f
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-29 00:09:29 +0300

    Add files via upload

---
+++ Lesson 3 Arrays/Rounding_Numbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inputNumbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            int[] roundedNumbers = new int[inputNumbers.Length];
+
+            for (int i = 0; i < roundedNumbers.Length; i++)
+            {
+                roundedNumbers[i] = (int)Math.Round(inputNumbers[i], MidpointRounding.AwayFromZero);
+            }
+
+            for (int i = 0; i < roundedNumbers.Length; i++)
+            {
+                Console.WriteLine($"{inputNumbers[i]} => {roundedNumbers[i]}");
+            }
+        }
+    }
+}
