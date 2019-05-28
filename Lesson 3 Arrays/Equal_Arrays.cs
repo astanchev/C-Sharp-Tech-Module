f9cf979e88892100d5246708bfaf5f55b8a6a50e
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-29 00:16:24 +0300

    Add files via upload

---
+++ Lesson 3 Arrays/Equal_Arrays.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Equal_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] array2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            if (array1.Length != array2.Length)
+            {
+                Console.WriteLine("Arrays are not identical. They have different lenght.");
+                return;
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    return;
+                }
+                else
+                {
+                    sum += array1[i];
+                }
+            }
+
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+
+        }
+    }
+}
