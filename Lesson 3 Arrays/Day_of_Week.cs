c433c7589584198e605e79272d32253177c85499
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-28 07:14:51 +0300

    Add files via upload

---
+++ Lesson 3 Arrays/Day_of_Week.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekDays = { "Monday", "Tuesday", "Wednesday",
+                                    "Thursday", "Friday",
+                                    "Saturday", "Sunday"};
+
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            if (inputNumber>=1&& inputNumber <= 7)
+            {
+                Console.WriteLine(weekDays[inputNumber-1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
