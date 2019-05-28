37ef1e3a4ba0cb64e3da104ab7ff8c2176abb3d7
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-28 07:16:37 +0300

    Add files via upload

---
+++ Lesson 3 Arrays/Print_Numbers_in_Reverse_Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02._Print_Numbers_in_Reverse_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumbers = int.Parse(Console.ReadLine());
+
+            int[] array = new int[inputNumbers];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+               array[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = array.Length-1; i >= 0; i--)
+            {
+                Console.Write($"{array[i]} ");
+            }
+        }
+    }
+}
