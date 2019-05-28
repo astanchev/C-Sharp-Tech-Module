e563ad0de68668eee695cf1c35b2382b13a67253
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-29 00:11:51 +0300

    Add files via upload

---
+++ Lesson 3 Arrays/Fold_and_Sum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] firstRow = new int[inputArray.Length / 2];
+            int[] secondRow = new int[inputArray.Length / 2];
+
+            for (int i = 0; i < inputArray.Length / 4; i++)
+            {
+                firstRow[i] = inputArray[inputArray.Length / 4 - 1 - i];
+            }
+            for (int i = 0; i < secondRow.Length; i++)
+            {
+                secondRow[i] = inputArray[inputArray.Length / 4 + i];
+            }
+            for (int i = firstRow.Length / 2, j = 0; i < firstRow.Length; i++, j++)
+            {
+                firstRow[i] = inputArray[inputArray.Length - 1 - j];
+            }
+            
+            for (int i = 0; i < firstRow.Length; i++)
+            {
+                Console.Write(firstRow[i]+secondRow[i]+" ");
+            }
+        }
+    }
+}
