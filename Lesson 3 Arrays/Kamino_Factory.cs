0112875a8cb3d182afea9220375615b7394b631b
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-29 00:18:33 +0300

    Add files via upload

---
+++ Lesson 3 Arrays/Kamino_Factory.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._Kamino_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int[] bestDNA = new int[length];
+            int bestLenghtOfOnes = -1;
+            int bestFirstIndex = -1;
+            int bestSumDNA = 0;
+            int bestSampleIndex = 0;
+            int currentSampleIndex = 0;
+
+            while (true)
+            {
+                string dnaSample = Console.ReadLine();
+                if (dnaSample == "Clone them!")
+                {
+                    break;
+                }
+
+                int[] dnaLine = dnaSample.Split(new char[] { '!' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                //Тази форма на Split се използва защото '!' могат да бъдат повече от един
+                int maxCurrentLenghtOfOnes = 0;
+                int currentEndIndex = 0;                
+                int currentLenghtOfOnes = 0;
+
+                for (int i = 0; i < dnaLine.Length; i++)
+                {
+                    if (dnaLine[i] == 1)
+                    {
+                        currentLenghtOfOnes++;
+                        if (currentLenghtOfOnes > maxCurrentLenghtOfOnes)
+                        {
+                            currentEndIndex = i;
+                            maxCurrentLenghtOfOnes = currentLenghtOfOnes;                            
+                        }
+                    }
+                    else
+                    {
+                        currentLenghtOfOnes = 0;
+                    }
+                }
+
+                int currentStartIndex = currentEndIndex - maxCurrentLenghtOfOnes + 1;
+
+                bool isCurrentDnaBetter = false;
+                int currentSumDNA = dnaLine.Sum();
+                
+                if (maxCurrentLenghtOfOnes > bestLenghtOfOnes)
+                {
+                    isCurrentDnaBetter = true;
+                }
+                else if (maxCurrentLenghtOfOnes == bestLenghtOfOnes)
+                {
+                    if (currentStartIndex < bestFirstIndex)
+                    {
+                        isCurrentDnaBetter = true;
+                    }
+                    else if (currentStartIndex == bestFirstIndex)
+                    {
+                        if (currentSumDNA > bestSumDNA)
+                        {
+                            isCurrentDnaBetter = true;
+                        }
+                    }
+                }
+                
+                currentSampleIndex++;
+
+                if (isCurrentDnaBetter)
+                {
+                    bestDNA = dnaLine;
+                    bestLenghtOfOnes = maxCurrentLenghtOfOnes;
+                    bestFirstIndex = currentStartIndex;
+                    bestSumDNA = currentSumDNA;
+                    bestSampleIndex = currentSampleIndex;
+                }                
+            }
+            Console.WriteLine($"Best DNA sample {bestSampleIndex} with sum: {bestSumDNA}.");
+            Console.WriteLine(string.Join(" ", bestDNA));
+        }
+    }
+}
---
+++ Lesson 3 Arrays/LadyBugs.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Linq;
+
+namespace _10._LadyBugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfField = int.Parse(Console.ReadLine());
+            int[] inputPositions = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int[] realPositions = new int[sizeOfField];
+            bool[] ladyBugPosition = new bool[sizeOfField];
+
+            for (int i = 0; i < sizeOfField; i++)
+            {
+                for (int j = 0; j < inputPositions.Length; j++)
+                {
+                    if (i == inputPositions[j])
+                    {
+                        realPositions[i] = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < sizeOfField; i++)
+            {
+                if (realPositions[i] == 1)
+                {
+                    ladyBugPosition[i] = true;
+                }
+                else
+                {
+                    ladyBugPosition[i] = false;
+                }
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "end")
+                {
+                    break;
+                }
+
+                string[] commandLine = command.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                int ladyBugStartPosition = int.Parse(commandLine[0]);
+                string ladyBugDirection = commandLine[1];
+                int ladyBugFlyLenght = int.Parse(commandLine[2]);
+
+
+                if (ladyBugStartPosition > ladyBugPosition.Length - 1 || ladyBugStartPosition < 0)
+                {
+                    continue;
+                }
+                else if (ladyBugPosition[ladyBugStartPosition] == false)
+                {
+                    continue;
+                }
+                else
+                {
+                    ladyBugPosition[ladyBugStartPosition] = false;
+                    realPositions[ladyBugStartPosition] = 0;
+                    if (ladyBugDirection == "right")
+                    {
+                        ladyBugStartPosition += ladyBugFlyLenght;
+                        if (ladyBugStartPosition < ladyBugPosition.Length)
+                        {
+                            if (ladyBugPosition[ladyBugStartPosition] == false)
+                            {
+                                ladyBugPosition[ladyBugStartPosition] = true;
+                                realPositions[ladyBugStartPosition] = 1;
+                            }
+                            else
+                            {
+                                while (true)
+                                {
+                                    ladyBugStartPosition += ladyBugFlyLenght;
+                                    if (ladyBugStartPosition > ladyBugPosition.Length - 1)
+                                    {
+                                        break;
+                                    }
+                                    else if (ladyBugPosition[ladyBugStartPosition] == false)
+                                    {
+                                        ladyBugPosition[ladyBugStartPosition] = true;
+                                        realPositions[ladyBugStartPosition] = 1;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else if (ladyBugDirection == "left")
+                    {
+                        ladyBugStartPosition -= ladyBugFlyLenght;
+                        if (ladyBugStartPosition >= 0)
+                        {
+                            if (ladyBugPosition[ladyBugStartPosition] == false)
+                            {
+                                ladyBugPosition[ladyBugStartPosition] = true;
+                                realPositions[ladyBugStartPosition] = 1;
+                            }
+                            else
+                            {
+                                while (true)
+                                {
+                                    ladyBugStartPosition -= ladyBugFlyLenght;
+                                    if (ladyBugStartPosition < 0)
+                                    {
+                                        break;
+                                    }
+                                    else if (ladyBugPosition[ladyBugStartPosition] == false)
+                                    {
+                                        ladyBugPosition[ladyBugStartPosition] = true;
+                                        realPositions[ladyBugStartPosition] = 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < realPositions.Length; i++)
+            {
+                Console.Write($"{realPositions[i]} ");
+            }
+        }
+    }
+}
