3b317369e5fd4300cc504d35312dbb97c21496fc
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-29 00:12:14 +0300

    Add files via upload

---
+++ Lesson 3 Arrays/Reverse_Array_of_Strings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayOfStrings = Console.ReadLine().Split(' ').ToArray();
+            string[] reversedArray = new string[arrayOfStrings.Length];
+
+            for (int i = 0; i < reversedArray.Length; i++)
+            {
+                reversedArray[i] = arrayOfStrings[arrayOfStrings.Length - 1 - i];
+            }
+
+            Console.WriteLine(string.Join(" ", reversedArray));
+        }
+    }
+}
