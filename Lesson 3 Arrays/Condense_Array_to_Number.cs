141fb80509b2aae8f44dfccfe06cec53f0fb0df5
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-29 00:17:18 +0300

    Add files via upload

---
+++ Lesson 3 Arrays/Condense_Array_to_Number.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            if (array.Length == 1)
+            {
+                Console.WriteLine($"{array[0]}");
+                return;
+            }
+
+
+            while (array.Length != 1)
+            {
+                int[] temp = new int[array.Length - 1];
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    temp[i] = array[i] + array[i + 1];
+                }
+                array = temp;
+                
+            }
+
+            Console.WriteLine(array[0]);
+        }
+    }
+}
