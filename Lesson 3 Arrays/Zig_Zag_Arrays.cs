2d8c24db99547b421f918951e9af784746a9ccbe
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-29 00:09:54 +0300

    Add files via upload

---
+++ Lesson 3 Arrays/Zig_Zag_Arrays.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Zig_Zag_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberLines = int.Parse(Console.ReadLine());
+
+            int[] firstArray = new int[numberLines];
+            int[] secondArray = new int[numberLines];
+
+            for (int i = 1; i <= numberLines; i++)
+            {
+                int[] temp = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                if (i % 2 != 0)
+                {
+                    firstArray[i - 1] = temp[0];
+                    secondArray[i - 1] = temp[1];
+                }
+                else
+                {
+                    firstArray[i - 1] = temp[1];
+                    secondArray[i - 1] = temp[0];
+                }
+            }
+            Console.WriteLine(string.Join(" ", firstArray));
+            Console.WriteLine(string.Join(" ", secondArray));
+        }
+    }
+}
