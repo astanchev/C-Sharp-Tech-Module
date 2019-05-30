af3f82bdf81450af53440c48ffc14dda9fab73a0
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-30 07:41:07 +0300

    Add files via upload

---
+++ Lesson 4 Methods/Characters_in_Range.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _03._Characters_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            char smallChar = SmallCharacter(firstChar, secondChar);
+            char bigChar = BigCharacter(firstChar, secondChar);
+            PrintAllCharctersBetween(smallChar, bigChar);
+
+        }
+
+        static char SmallCharacter(char firstChar, char secondChar)
+        {
+            if (firstChar<secondChar)
+            {
+                return firstChar;
+            }
+            else
+            {
+                return secondChar;
+            }
+        }
+
+        static char BigCharacter(char firstChar, char secondChar)
+        {
+            if (firstChar > secondChar)
+            {
+                return firstChar;
+            }
+            else
+            {
+                return secondChar;
+            }
+        }
+
+        static void PrintAllCharctersBetween(char smallChar, char bigChar)
+        {
+            for (int i = smallChar + 1; i < bigChar; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
