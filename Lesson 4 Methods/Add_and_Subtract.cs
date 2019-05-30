81a3553f989339f7e4d11cbb332464d2ecdf0ed8
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-30 07:44:15 +0300

    Add files via upload

---
+++ Lesson 4 Methods/Add_and_Subtract.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int result = Subtract( Add(firstNumber, secondNumber), thirdNumber);
+            Console.WriteLine(result);
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Subtract(int sum, int c)
+        {
+            return sum - c;
+        }
+    }
+}
