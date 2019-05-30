454e8e6124a3699cc3d78b942442e36cee89801b
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-30 07:40:27 +0300

    Add files via upload

---
+++ Lesson 4 Methods/Vowels_Count_2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+
+            int result = CountVowels(inputString);
+            Console.WriteLine(result);
+        }
+
+        static int CountVowels(string inputString)
+        {
+            int sumVowels = 0;
+            char[] vowels = { 'a', 'o', 'e', 'u', 'i',
+                              'A', 'O', 'E', 'U', 'I'};
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (vowels.Contains(inputString[i]))
+                {
+                    sumVowels++;
+                }
+            }
+
+            return sumVowels;
+        }
+    }
+}
