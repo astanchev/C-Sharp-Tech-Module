f654d496f4621f52a7b839c679097a96cd8583a7
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-30 07:38:06 +0300

    Add files via upload

---
+++ Lesson 4 Methods/Data_Types.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01._Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfData = Console.ReadLine();
+            switch (typeOfData)
+            {
+                case "int": PrintResult(int.Parse(Console.ReadLine())); break;
+                case "real": PrintResult(double.Parse(Console.ReadLine())); break;
+                case "string": PrintResult(Console.ReadLine()); break;
+                default: break;
+            }
+        }
+
+        static void PrintResult(int number)
+        {
+            Console.WriteLine(number*2);
+        }
+
+        static void PrintResult(double number)
+        {
+            Console.WriteLine($"{number*1.5:f2}");
+        }
+
+        static void PrintResult(string word)
+        {
+            Console.WriteLine($"${word}$");
+        }
+    }
+}
