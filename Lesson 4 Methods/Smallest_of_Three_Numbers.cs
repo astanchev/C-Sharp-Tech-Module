496eb9222bc16291b0fc14913a0bb4498ddd4dd6
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-30 07:39:01 +0300

    Add files via upload

---
+++ Lesson 4 Methods/Smallest_of_Three_Numbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._Smallest_of_Three_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int result = SmalestNumber(firstNumber, secondNumber,thirdNumber);
+            Console.WriteLine(result);
+        }
+
+        static int SmalestNumber(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            int result = firstNumber;
+
+            if (result >= secondNumber)
+            {
+                result = secondNumber;
+            }
+            if (result >= thirdNumber)
+            {
+                result = thirdNumber;
+            }            
+            return result;
+        }
+    }
+}
