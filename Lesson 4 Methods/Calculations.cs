55341a0be320f1fae987067754be4aa6c225ce95
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-30 07:40:48 +0300

    Add files via upload

---
+++ Lesson 4 Methods/Calculations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _03._Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string calculation = Console.ReadLine();
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            switch (calculation)
+            {
+                case "add": Add(firstNumber, secondNumber); break;
+                case "multiply": Multiply(firstNumber, secondNumber); break;
+                case "subtract": Subtract(firstNumber, secondNumber); break;
+                case "divide": Divide(firstNumber, secondNumber); break;
+                default: break;
+            }
+        }
+
+        static void Add(double a, double b)
+        {
+            Console.WriteLine(a+b);
+        }
+
+        static void Multiply(double a, double b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        static void Subtract(double a, double b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        static void Divide(double a, double b)
+        {
+            Console.WriteLine(a / b);
+        }
+    }
+}
