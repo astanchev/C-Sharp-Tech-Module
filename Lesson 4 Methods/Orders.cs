5550dd0ffa4a89aefa20c8e6e08b3da7c7eef707
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-30 07:45:25 +0300

    Add files via upload

---
+++ Lesson 4 Methods/Orders.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string order = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            OrderPrice(order, quantity);
+        }
+
+        static void OrderPrice(string product, int quantity)
+        {
+            double price = 0.0;
+            switch (product)
+            {
+                case "coffee": price = 1.50; break;
+                case "water": price = 1.00; break;
+                case "coke": price = 1.40; break;
+                case "snacks": price = 2.00; break;
+                default: break;
+            }
+            Console.WriteLine($"{price * quantity:f2}");
+        }
+    }
+}
