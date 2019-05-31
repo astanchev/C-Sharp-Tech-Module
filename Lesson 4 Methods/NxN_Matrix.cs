e3bc3317bda7f47f787219a06cee93cc05936855
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-31 21:02:00 +0300

    Add files via upload

---
+++ Lesson 4 Methods/NxN_Matrix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07._NxN_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintMatrix(number);
+        }
+
+        static void PrintMatrix(int n)
+        {
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= n; col++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
