6dbc85a6746b78150a92a8412076aae5774472b7
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-31 21:06:38 +0300

    Add files via upload

---
+++ Lesson 4 Methods/Palindrome_Integers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._Palindrome_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input!="END")
+            {
+                PalindromeCheck(input);
+                input = Console.ReadLine();
+            }
+        }
+
+        static void PalindromeCheck(string input)
+        {
+            bool isPalindrome = true;
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i]!=input[input.Length-1-i])
+                {
+                    isPalindrome = false;
+                }
+            }
+            Console.WriteLine(isPalindrome.ToString().ToLower());
+        }
+    }
+}
