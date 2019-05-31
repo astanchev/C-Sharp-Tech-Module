a5dc7a42f458840b0eca616d90547bd2d8927daf
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-31 21:02:37 +0300

    Add files via upload

---
+++ Lesson 4 Methods/Repeat_String.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            int repeatNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatString(inputString, repeatNumber));
+        }
+
+        static string RepeatString(string inputString, int repeatNumber)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < repeatNumber; i++)
+            {
+                result += inputString;
+            }
+
+            return result;
+        }
+    }
+}
