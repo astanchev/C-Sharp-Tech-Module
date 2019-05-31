bb7e3707ac4103720638c030cc09be759df6837a
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-31 21:04:18 +0300

    Add files via upload

---
+++ Lesson 4 Methods/Factorial_Division.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long firstNumber = long.Parse(Console.ReadLine());
+            long secondNumber = long.Parse(Console.ReadLine());
+            double result = Devide(Factorial(firstNumber), Factorial(secondNumber));
+            Console.WriteLine($"{result:f2}");
+        }
+
+        static long Factorial(long n)
+        {
+            long factorial = 1;
+            for (long i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        static double Devide(long a, long b)
+        {
+            double result = (double)a / b;
+            return result;
+        }
+    }
+}
