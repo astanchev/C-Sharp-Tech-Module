403313088d74621bddf80198e40b8038a0ec54ad
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-31 21:00:39 +0300

    Add files via upload

---
+++ Lesson 4 Methods/Middle_Characters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06._Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            PrintMiddleCharacter(input);
+        }
+
+        static void PrintMiddleCharacter(string input)
+        {
+            if (input.Length%2!=0)
+            {
+                Console.WriteLine(input[input.Length/2]);
+            }
+            else
+            {
+                Console.WriteLine($"{input[input.Length / 2 - 1]}{input[input.Length / 2]}");
+            }
+        }
+    }
+}
