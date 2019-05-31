95cda494d7648e00e9db17f8162720c677bc12e8
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-31 21:10:21 +0300

    Add files via upload

---
+++ Lesson 4 Methods/Math_operations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            string calculation = Console.ReadLine();
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            double result = Calculate(firstNumber, calculation, secondNumber);
+            Console.WriteLine(result);
+        }
+
+        static double Calculate(int a, string @operation, int b)
+        {
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+": result = a + b; break;
+                case "-": result = a - b; break;
+                case "*": result = a * b; break;
+                case "/": result = a * 1.0 / b; break;
+                default: break;
+            }
+
+            return result;
+        }
+    }
+}
