8a778e0eb76032634f78fa1d84620bd20c16cc50
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-31 21:07:26 +0300

    Add files via upload

---
+++ Lesson 4 Methods/Multiply_Evens_by_Odds.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _10._Multiply_Evens_by_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = Math.Abs(int.Parse(Console.ReadLine()));
+
+            int multiple = GetMultipleOfEvenAndOdds(inputNumber);
+
+            Console.WriteLine(multiple);
+        }
+
+        static int GetMultipleOfEvenAndOdds(int inputNumber)
+        {
+            int result = 0;
+            int evenSum = 0;
+            int oddSum = 0;
+            while (inputNumber != 0)
+            {
+                int digit = inputNumber % 10;
+                if (digit%2==0)
+                {
+                    evenSum += digit;
+                }
+                else
+                {
+                    oddSum += digit;
+                }
+                inputNumber /= 10;
+            }
+            result = evenSum * oddSum;
+            return result;
+        }
+    }
+}
