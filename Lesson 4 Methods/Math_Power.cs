6f0cf42f91d1b78d986536b99b1aadd574fc2d0b
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-31 21:05:19 +0300

    Add files via upload

---
+++ Lesson 4 Methods/Math_Power.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            double result = RaiseToPower(number, power);
+            Console.WriteLine(result);
+        }
+
+        static double RaiseToPower(double number, int power)
+        {
+            double result = 1.0;
+
+            for (int i = 1; i <= power; i++)
+            {
+                result *= number;
+            }
+            return result;
+        }
+    }
+}
