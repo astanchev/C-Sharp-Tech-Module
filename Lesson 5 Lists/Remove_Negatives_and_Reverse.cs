60ba9b7899dde9359036a1282c9074fc1d49f46f
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-08 21:11:31 +0300

    Add files via upload

---
+++ Lesson 5 Lists/Remove_Negatives_and_Reverse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Remove_Negatives_and_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine()
+                                .Split(' ')
+                                .Select(int.Parse)
+                                .Where(x => x >= 0)
+                                .ToList();
+
+            if (input.Count>0)
+            {
+                input.Reverse();
+                Console.WriteLine(string.Join(" ", input));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+
+        }
+    }
+}
