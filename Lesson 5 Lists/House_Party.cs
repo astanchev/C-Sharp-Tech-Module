26409324c1039dd5fa88337b709c4f7710e898d0
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-08 21:14:28 +0300

    Add files via upload

---
+++ Lesson 5 Lists/House_Party.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._House_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            List<string> guests = new List<string>();
+                        
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                List<string> command = Console.ReadLine().Split().ToList();
+                string name = command[0];
+                if (command.Count>3)
+                {
+                    if (guests.Contains(name))
+                    {
+                        guests.Remove(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                    }
+                }
+                else
+                {
+                    if (guests.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                    }
+                    else
+                    {
+                        guests.Add(name);
+                    }
+                }
+            }
+
+            foreach (var guest in guests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}
