0e92124c288d5ad7cae7ac847a2f6f9dffee19a3
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-08 21:10:50 +0300

    Add files via upload

---
+++ Lesson 5 Lists/Messaging.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToList();
+
+            List<char> text = Console.ReadLine().ToList();
+            string result = string.Empty;
+            List<int> sumNumbers = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int number = numbers[i];
+                int sum = 0;
+                while (number != 0)
+                {
+                    sum += number % 10;
+                    number /= 10;
+                }
+                sumNumbers.Add(sum);
+            }
+
+            for (int i = 0; i < sumNumbers.Count; i++)
+            {
+                int elmentIndex = sumNumbers[i];
+                for (int j = 0; j < text.Count; j++)
+                {                    
+                    if (elmentIndex >= text.Count)
+                    {
+                        elmentIndex = elmentIndex - text.Count;
+                    }
+                    if (j == elmentIndex)
+                    {
+                        string letter = text[j].ToString();
+                        result += letter;
+                        text.RemoveAt(j);
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
