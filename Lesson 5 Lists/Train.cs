68be6024008a32995916b9d5c6ac3d3e44eed6b0
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-08 21:11:57 +0300

    Add files via upload

---
+++ Lesson 5 Lists/Train.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                                .Split(' ')
+                                .Select(int.Parse)
+                                .ToList();
+
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+                List<string> inputCommand = input.Split().ToList();
+                if (inputCommand.Count > 1)
+                {
+                    int passengersInNewWagon = int.Parse(inputCommand[1]);
+                    wagons.Add(passengersInNewWagon);
+                }
+                else
+                {
+                    int passengersToAdd = int.Parse(inputCommand[0]);
+
+                    for (int wagon = 0; wagon < wagons.Count; wagon++)
+                    {
+                        int availableSeats = maxCapacity - wagons[wagon];
+                        if (passengersToAdd <= availableSeats)
+                        {
+                            wagons[wagon] += passengersToAdd;
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
