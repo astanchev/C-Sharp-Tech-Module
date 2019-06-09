2cf70f7df89255da15cace1533e3e6f36a7a2ec0
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-09 21:22:07 +0300

    Add files via upload

---
+++ Lesson 5 Lists/Drum_Set.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Drum_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double savings = double.Parse(Console.ReadLine());
+            List<int> drumSetPowers = Console.ReadLine()
+                                          .Split()
+                                          .Select(int.Parse)
+                                          .ToList();
+            List<int> initialDrumSetPowers = new List<int>();
+            for (int i = 0; i < drumSetPowers.Count; i++)
+            {
+                initialDrumSetPowers.Add(drumSetPowers[i]);
+            }
+            
+            while (true)
+            {
+                string inputMassage = Console.ReadLine();
+                if (inputMassage == "Hit it again, Gabsy!")
+                {
+                    break;
+                }
+                int decreasepower = int.Parse(inputMassage);
+                for (int i = 0; i < drumSetPowers.Count; i++)
+                {
+                    int drumPower = drumSetPowers[i];
+                    if (decreasepower >= drumPower)
+                    {
+                        int priceForRepair = initialDrumSetPowers[i] * 3;
+                        if (priceForRepair <= savings)
+                        {
+                            drumSetPowers[i] = initialDrumSetPowers[i];
+                            savings -= priceForRepair;
+                        }
+                        else
+                        {
+                            drumSetPowers.RemoveAt(i);
+                            initialDrumSetPowers.RemoveAt(i);
+                            i--;
+                        }
+                    }
+                    else
+                    {
+                        drumSetPowers[i] -= decreasepower;
+                    }
+                }
+
+
+
+            }
+
+            Console.WriteLine(string.Join(" ", drumSetPowers));
+            Console.WriteLine($"Gabsy has {savings:f2}lv.");
+        }
+    }
+}
