7397a98b40348ac6db95f1ea17cb89b289e4e53d
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-09 21:20:40 +0300

    Add files via upload

---
+++ Lesson 5 Lists/List_of_Products.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfProducts = int.Parse(Console.ReadLine());
+            List<string> listOfProducts = new List<string>();
+
+            for (int i = 0; i < numberOfProducts; i++)
+            {
+                string product = Console.ReadLine();
+                listOfProducts.Add(product);
+            }
+            listOfProducts.Sort();
+
+            for (int i = 0; i < listOfProducts.Count; i++)
+            {
+                Console.WriteLine($"{i+1}.{listOfProducts[i]}");
+            }
+        }
+    }
+}
