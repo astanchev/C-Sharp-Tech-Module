6a9b014cbcf94ba4e4a3426704fecf05d7fa0ae1
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-09 21:23:48 +0300

    Add files via upload

---
+++ Lesson 5 Lists/Append_Arrays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> arrays = Console.ReadLine()
+                                    .Split('|')
+                                    .ToList();
+
+            List<int> result = new List<int>();
+
+            for (int i = arrays.Count - 1; i >= 0 ; i--)
+            {
+                string arrayString = arrays[i];
+                List<int> temp = arrayString
+                                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                .Select(int.Parse)
+                                .ToList();
+                result.AddRange(temp);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
