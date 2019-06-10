cce1f53d18702225af3375d5db5b431bd3d79b51
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-10 07:59:18 +0300

    Add files via upload

---
+++ Lesson 6 Dictionaries/Count_Chars_in_a_String.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] text = Console.ReadLine().Where(x => x != ' ').ToArray();
+            Dictionary<char, int> countChars = new Dictionary<char, int>();
+            foreach (var @char in text)
+            {
+                if (!countChars.ContainsKey(@char))
+                {
+                    countChars[@char] = 0;
+                }
+                countChars[@char]++;
+            }
+
+            foreach (var kvp in countChars)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
