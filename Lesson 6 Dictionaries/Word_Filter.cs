932a88fcfa2ef5a96984b0f81e951109eac1f70c
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-10 08:19:11 +0300

    Add files via upload

---
+++ Lesson 6 Dictionaries/Word_Filter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine()
+                             .Split()
+                             .Where(w => w.Length % 2 == 0)
+                             .ToArray();
+
+            foreach (var word in array)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
