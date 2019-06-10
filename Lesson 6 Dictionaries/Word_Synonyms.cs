bfc2c499845c59d39f0104cde2446806f9d9f917
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-10 08:17:27 +0300

    Add files via upload

---
+++ Lesson 6 Dictionaries/Word_Synonyms.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!words.ContainsKey(word))
+                {
+                    words[word] = new List<string>();
+                }
+                words[word].Add(synonym);
+            }
+
+            foreach (var kvp in words)
+            {
+                Console.WriteLine($"{kvp.Key} - {string.Join(", ", kvp.Value)}");
+            }
+        }
+    }
+}
