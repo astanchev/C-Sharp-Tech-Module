5b10628391bd71aa32e0478e15d6f1f2761f4af6
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-10 08:16:43 +0300

    Add files via upload

---
+++ Lesson 6 Dictionaries/A_Miner_Task.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resources = new Dictionary<string, int>();
+            while (true)
+            {
+                string resource = Console.ReadLine();
+                if (resource == "stop")
+                {
+                    break;
+                }
+                int quantity = int.Parse(Console.ReadLine());
+                if (!resources.ContainsKey(resource))
+                {
+                    resources[resource] = quantity;
+                }
+                else
+                {
+                    resources[resource] += quantity;
+                }
+            }
+
+            foreach (var kvp in resources)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
