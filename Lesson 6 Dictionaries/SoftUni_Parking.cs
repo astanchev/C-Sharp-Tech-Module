e4d5f272b84ef92121968bdb1519043a4f3d6d76
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-11 00:24:08 +0300

    Add files via upload

---
+++ Lesson 6 Dictionaries/SoftUni_Parking.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._SoftUni_Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var userPlateNumber = new Dictionary<string, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputLine = Console.ReadLine().Split();
+                string command = inputLine[0];
+                string username = inputLine[1];
+
+                if (command == "register")
+                {
+                    string licensePlateNumber = inputLine[2];
+                    if (!userPlateNumber.ContainsKey(username))
+                    {
+                        userPlateNumber[username] = licensePlateNumber;
+                        Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {userPlateNumber[username]}");
+                    }
+                }
+                else if (command == "unregister")
+                {
+                    if (userPlateNumber.ContainsKey(username))
+                    {
+                        userPlateNumber.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                }
+            }
+
+            foreach (var kvp in userPlateNumber)
+            {
+                Console.WriteLine($"{kvp.Key} => {kvp.Value}");
+            }
+        }
+    }
+}
