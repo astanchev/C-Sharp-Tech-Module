528b581ea2222065fc938c2970cd5f314fe5d432
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-16 23:00:25 +0300

    Add files via upload

---
+++ Lesson 7 Objects and Classes/Randomize_Words.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Randomize_Words
+{
+    class Program
+    {
+        static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            string[] inputWords = Console.ReadLine()
+                                        .Split()
+                                        .ToArray();
+
+            for (int i = 0; i < inputWords.Length; i++)
+            {
+                int elementIndex = i;
+                int randomPosition = rnd.Next(0, inputWords.Length);
+                Swap(elementIndex, randomPosition, inputWords);
+            }
+
+            foreach (var word in inputWords)
+            {
+                Console.WriteLine(word);
+            }
+
+        }
+
+        private static void Swap(int index1, int index2, string[] inputWords)
+        {
+            string oldWord = inputWords[index1];
+            inputWords[index1] = inputWords[index2];
+            inputWords[index2] = oldWord;
+        }
+    }
+}
