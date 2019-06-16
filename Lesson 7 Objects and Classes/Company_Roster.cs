d1fe883c14c763d5db9c9118442cbb6fbac946c6
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-16 22:58:14 +0300

    Add files via upload

--- Lesson 7 Objects and Classes/Company_Roster.cs
+++ Lesson 7 Objects and Classes/Company_Roster.cs
@@ -6,64 +6,82 @@ namespace _01._Company_Roster
 {
     class Employee
     {
-        public Employee(string name, decimal salary, string department)
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+
+        public Employee(string name, decimal salary)
         {
             this.Name = name;
             this.Salary = salary;
-            this.Department = department;
         }
+    }
 
-        public Employee()
+    class Department
+    {
+        public string DepartmentName { get; set; }
+        public List<Employee> Employees { get; set; }
+        public decimal TotalSalaries { get; set; }
+
+        public void AddNewEmployee(string empName, decimal empSalary)
         {
+            this.TotalSalaries += empSalary;
 
+            this.Employees.Add(new Employee(empName, empSalary));
         }
 
-        public string Name { get; set; }
-
-        public decimal Salary { get; set; }
-
-        public string Department { get; set; }
+        public Department(string departmentName)
+        {
+            this.Employees = new List<Employee>();
+            this.DepartmentName = departmentName;
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            List<Employee> employees = new List<Employee>();
+            List<Department> departments = new List<Department>();
 
             int numberOfEmploees = int.Parse(Console.ReadLine());
 
-            AddEmploeesToList(numberOfEmploees, employees);
+            AddEmploeesToList(numberOfEmploees, departments);
 
-            var result = employees.GroupBy(e => e.Department)
-                                  .Select(e => new
-                                  {
-                                      Department = e.Key,
-                                      AverageSalary = e.Average(emp => emp.Salary),
-                                      Employees = e.OrderByDescending(emp => emp.Salary)
-                                  })
-                                  .OrderByDescending(e => e.AverageSalary)
-                                  .FirstOrDefault();
+            Department bestDepartment = GetBestDepartment(departments);
 
-            Console.WriteLine($"Highest Average Salary: {result.Department}");
+            PrintBestDepartment(bestDepartment);
 
-            foreach (var employee in result.Employees)
+        }
+
+        private static void PrintBestDepartment(Department bestDepartment)
+        {
+            Console.WriteLine($"Highest Average Salary: {bestDepartment.DepartmentName}");
+
+            foreach (var employee in bestDepartment.Employees.OrderByDescending(e => e.Salary))
             {
                 Console.WriteLine($"{employee.Name} {employee.Salary:F2}");
             }
         }
-                
 
-        private static void AddEmploeesToList(int numberOfEmploees, List<Employee> employees)
+        private static Department GetBestDepartment(List<Department> departments)
+        {
+            return departments.OrderByDescending(d => d.TotalSalaries / d.Employees.Count).First();
+        }
+
+        private static void AddEmploeesToList(int numberOfEmploees, List<Department> departments)
         {
             for (int i = 0; i < numberOfEmploees; i++)
             {
-                string[] inputEmploee = Console.ReadLine().Split();
-                string name = inputEmploee[0];
-                decimal salary = decimal.Parse(inputEmploee[1]);
-                string department = inputEmploee[2];
-                Employee newEmploee = new Employee(name, salary, department);
-                employees.Add(newEmploee);
+                string[] inputEmployee = Console.ReadLine().Split();
+                string name = inputEmployee[0];
+                decimal salary = decimal.Parse(inputEmployee[1]);
+                string department = inputEmployee[2];
+                if (!departments.Any(d => d.DepartmentName == department))
+                {
+                    departments.Add(new Department(department));
+                }
+
+                departments.Find(d => d.DepartmentName == department)
+                           .AddNewEmployee(name, salary);
             }
         }
 
