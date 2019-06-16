5c198ff6a927393a2d61319d56e5520a6db72eaa
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-16 22:59:19 +0300

    Add files via upload

--- Lesson 7 Objects and Classes/Company_Roster.cs
+++ Lesson 7 Objects and Classes/Company_Roster.cs
@@ -6,82 +6,64 @@ namespace _01._Company_Roster
 {
     class Employee
     {
-        public string Name { get; set; }
-        public decimal Salary { get; set; }
-
-        public Employee(string name, decimal salary)
+        public Employee(string name, decimal salary, string department)
         {
             this.Name = name;
             this.Salary = salary;
+            this.Department = department;
         }
-    }
 
-    class Department
-    {
-        public string DepartmentName { get; set; }
-        public List<Employee> Employees { get; set; }
-        public decimal TotalSalaries { get; set; }
-
-        public void AddNewEmployee(string empName, decimal empSalary)
+        public Employee()
         {
-            this.TotalSalaries += empSalary;
 
-            this.Employees.Add(new Employee(empName, empSalary));
         }
 
-        public Department(string departmentName)
-        {
-            this.Employees = new List<Employee>();
-            this.DepartmentName = departmentName;
-        }
+        public string Name { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public string Department { get; set; }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            List<Department> departments = new List<Department>();
+            List<Employee> employees = new List<Employee>();
 
             int numberOfEmploees = int.Parse(Console.ReadLine());
 
-            AddEmploeesToList(numberOfEmploees, departments);
+            AddEmploeesToList(numberOfEmploees, employees);
 
-            Department bestDepartment = GetBestDepartment(departments);
+            var result = employees.GroupBy(e => e.Department)
+                                  .Select(e => new
+                                  {
+                                      Department = e.Key,
+                                      AverageSalary = e.Average(emp => emp.Salary),
+                                      Employees = e.OrderByDescending(emp => emp.Salary)
+                                  })
+                                  .OrderByDescending(e => e.AverageSalary)
+                                  .FirstOrDefault();
 
-            PrintBestDepartment(bestDepartment);
+            Console.WriteLine($"Highest Average Salary: {result.Department}");
 
-        }
-
-        private static void PrintBestDepartment(Department bestDepartment)
-        {
-            Console.WriteLine($"Highest Average Salary: {bestDepartment.DepartmentName}");
-
-            foreach (var employee in bestDepartment.Employees.OrderByDescending(e => e.Salary))
+            foreach (var employee in result.Employees)
             {
                 Console.WriteLine($"{employee.Name} {employee.Salary:F2}");
             }
         }
+                
 
-        private static Department GetBestDepartment(List<Department> departments)
-        {
-            return departments.OrderByDescending(d => d.TotalSalaries / d.Employees.Count).First();
-        }
-
-        private static void AddEmploeesToList(int numberOfEmploees, List<Department> departments)
+        private static void AddEmploeesToList(int numberOfEmploees, List<Employee> employees)
         {
             for (int i = 0; i < numberOfEmploees; i++)
             {
-                string[] inputEmployee = Console.ReadLine().Split();
-                string name = inputEmployee[0];
-                decimal salary = decimal.Parse(inputEmployee[1]);
-                string department = inputEmployee[2];
-                if (!departments.Any(d => d.DepartmentName == department))
-                {
-                    departments.Add(new Department(department));
-                }
-
-                departments.Find(d => d.DepartmentName == department)
-                           .AddNewEmployee(name, salary);
+                string[] inputEmploee = Console.ReadLine().Split();
+                string name = inputEmploee[0];
+                decimal salary = decimal.Parse(inputEmploee[1]);
+                string department = inputEmploee[2];
+                Employee newEmploee = new Employee(name, salary, department);
+                employees.Add(newEmploee);
             }
         }
 
---
+++ Lesson 7 Objects and Classes/Day_of_Week.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01._Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            string input = Console.ReadLine();
+            DateTime date = DateTime.ParseExact(input, "d-M-yyyy", provider);
+            Console.WriteLine(date.DayOfWeek);
+
+        }
+    }
+}
