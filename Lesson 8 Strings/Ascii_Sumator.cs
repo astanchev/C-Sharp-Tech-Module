1dbbaf8c870c8a93740fdf74523085491a34df65
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-24 21:53:32 +0300

    Add files via upload

---
+++ Lesson 8 Strings/Ascii_Sumator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02._Ascii_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int sum = 0;
+
+            if (first > second)
+            {
+                char temp = first;
+                first = second;
+                second = temp;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char symb = input[i];
+                if (symb > first && symb < second)
+                {
+                    sum += symb;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
