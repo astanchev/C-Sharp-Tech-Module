a8c858da425182e80b41e74bae5f622125819f99
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-24 21:54:56 +0300

    Add files via upload

---
+++ Lesson 8 Strings/Extract_File.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int index = input.LastIndexOf('\\');
+            string file = input.Substring(index+1);
+            string[] fileParts = file.Split('.');
+            string name = fileParts[0];
+            string extension = fileParts[1];
+            Console.WriteLine($"File name: {name}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
