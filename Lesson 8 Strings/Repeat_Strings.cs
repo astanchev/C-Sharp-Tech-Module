4ae40aeb3ea58a2acc15d253c400e61791bdf74c
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-24 21:54:08 +0300

    Add files via upload

---
+++ Lesson 8 Strings/Repeat_Strings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._Repeat_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputLine = Console.ReadLine().Split();
+            for (int i = 0; i < inputLine.Length; i++)
+            {
+                string result = string.Empty;
+                for (int j = 0; j < inputLine[i].Length; j++)
+                {
+                    result += inputLine[i];
+                }
+                inputLine[i] = result;
+            }
+            Console.WriteLine(string.Join("", inputLine));
+        }
+    }
+}
