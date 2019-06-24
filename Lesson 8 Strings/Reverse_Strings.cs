45f4d33c51fa98203aa1d950a8634424d68b17da
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-24 21:52:53 +0300

    Add files via upload

---
+++ Lesson 8 Strings/Reverse_Strings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+                string result = string.Empty;
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    result += input[input.Length - 1 - i];
+                }
+                Console.WriteLine($"{input} = {result}");
+            }
+        }
+    }
+}
