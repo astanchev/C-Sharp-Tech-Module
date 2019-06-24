d598f1f889955a8d284ad1e27a9c2b836408eb1a
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-24 21:54:37 +0300

    Add files via upload

---
+++ Lesson 8 Strings/Repeat_Strings_2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace _02._Repeat_Strings_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputLine = Console.ReadLine().Split();
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (var word in inputLine)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    result.Append(word);
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
