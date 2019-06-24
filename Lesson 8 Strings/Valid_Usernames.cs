31ff6455fc1f35c42c2a41cba3f64de06556015f
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-24 21:53:12 +0300

    Add files via upload

---
+++ Lesson 8 Strings/Valid_Usernames.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ");
+
+            foreach (var name in input)
+            {
+                if (hasLenght(name) && isCorrect(name))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+
+        }
+
+        private static bool isCorrect(string name)
+        {
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (char.IsLetterOrDigit(name[i]) 
+                    || name[i] == '-' 
+                    || name[i] == '_')
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool hasLenght(string name)
+        {
+            if (name.Length >= 3 && name.Length <= 16)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
