b2cc0f0273bb40e87cbc8501b46dfb587e87bba7
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-25 14:21:41 +0300

    Add files via upload

---
+++ Lesson 8 Strings/Text_Filter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] separators = { ',', ' ' };
+            string[] bannedWords = Console.ReadLine()
+                                    .Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < bannedWords.Length; i++)
+            {
+                string bannedWord = bannedWords[i];
+                string replacement = new string('*', bannedWord.Length);
+                if (text.Contains(bannedWord))
+                {
+                    text = text.Replace(bannedWord, replacement);
+                }                
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
