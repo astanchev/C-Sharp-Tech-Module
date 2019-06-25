c6451f0c3be57648b0fb1a9bf7a65db7885a3eb3
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-25 14:22:02 +0300

    Add files via upload

---
+++ Lesson 8 Strings/Digits__Letters_and_Other.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace _05._Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder letters = new StringBuilder();
+            StringBuilder digits = new StringBuilder();
+            StringBuilder others = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char symb = text[i];
+                if (char.IsLetter(symb))
+                {
+                    letters.Append(symb);
+                }
+                else if (char.IsDigit(symb))
+                {
+                    digits.Append(symb);
+                }
+                else
+                {
+                    others.Append(symb);
+                }
+            }
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
