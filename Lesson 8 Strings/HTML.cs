a5926ee80d3e0270fa4fae51fa9cbc7a8be650c5
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-25 14:22:21 +0300

    Add files via upload

---
+++ Lesson 8 Strings/HTML.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05._HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = Console.ReadLine();
+            string content = Console.ReadLine();
+
+            List<string> comments = new List<string>();
+
+            while (true)
+            {
+                string inputLine = Console.ReadLine();
+                if (inputLine == "end of comments")
+                {
+                    break;
+                }
+                comments.Add(inputLine);
+            }
+            Console.WriteLine("<h1>");
+            Console.WriteLine($"\t{title}");
+            Console.WriteLine("</h1>");
+            Console.WriteLine("<article>");
+            Console.WriteLine($"\t{content}");
+            Console.WriteLine("</article>");
+            foreach (var line in comments)
+            {
+                Console.WriteLine("<div>");
+                Console.WriteLine($"\t{line}");
+                Console.WriteLine("</div>");
+            }
+        }
+    }
+}
