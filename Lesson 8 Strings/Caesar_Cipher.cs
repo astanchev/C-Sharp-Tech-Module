4d5411dd37326e09fbd585eb877d9e7e54b84437
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-25 14:21:00 +0300

    Add files via upload

---
+++ Lesson 8 Strings/Caesar_Cipher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _04._Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var symb in input)
+            {
+                char newSymb = (char)(symb + 3);
+                sb.Append(newSymb);
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
