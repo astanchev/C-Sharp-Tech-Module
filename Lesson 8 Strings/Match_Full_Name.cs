1811f0cbb0311c609b84cbde4f572b58e47f3742
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-25 14:23:12 +0300

    Add files via upload

---
+++ Lesson 8 Strings/Match_Full_Name.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _06._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();            
+
+            Regex regex = new Regex(@"\b[A-Z][a-z]+ [A-Z][a-z]+");
+
+            MatchCollection matches = regex.Matches(input);
+
+            List<string> result = new List<string>();
+
+            foreach (Match m in matches)
+            {
+                result.Add(m.Value);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+                                
+        }
+    }
+}
