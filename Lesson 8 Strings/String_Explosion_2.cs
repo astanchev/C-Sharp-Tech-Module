2cbcbfd1a3c1764a105bdbe74fe4d54020218518
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-28 07:51:04 +0300

    Add files via upload

---
+++ Lesson 8 Strings/String_Explosion_2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07._String_Explosion_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int power = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char symb = input[i];
+
+                if (symb == '>')
+                {
+                    power += input[i+1] - 48;
+                    continue;
+                }
+                if (power > 0)
+                {
+                    input = input.Remove(i, 1);
+                    i--;
+                    power--;
+                }
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
