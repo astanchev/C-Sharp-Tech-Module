131391035977239d0acd143efe41e4da0bbe6c8f
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-28 07:49:30 +0300

    Add files via upload

---
+++ Lesson 8 Strings/Match_Dates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _07._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(@"\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b");
+
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match m in matches)
+            {
+                string day = m.Groups["day"].Value;
+                string month = m.Groups["month"].Value;
+                string year = m.Groups["year"].Value;
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+            
+        }
+    }
+}
